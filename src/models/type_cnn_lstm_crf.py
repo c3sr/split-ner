9f159678de566675625678079ca315ce186b61af
Jatin Arora <jatinarora2702@gmail.com>
2020-11-25 19:45:49 -0600

    add config based args input

--- src/models/type_cnn_lstm_crf.py
+++ src/models/type_cnn_lstm_crf.py
@@ -12,7 +12,7 @@ from src.components.crf import CRF
 from src.models.base import BaseExecutor
 from src.reader.type_dataset import TypeDataset
 from src.utils.evaluator import Evaluator
-from src.utils.general import set_all_seeds
+from src.utils.general import set_all_seeds, parse_config
 
 
 class CNN_LSTM_CRF(nn.Module):
@@ -297,118 +297,14 @@ class TypeCNN_LSTM_CRFExecutor(BaseExecutor):
 
 
 def main(args):
-    set_all_seeds(args.seed)
-    executor = TypeCNN_LSTM_CRFExecutor(args)
+    config = parse_config(args.config)
+    set_all_seeds(config.seed)
+    executor = TypeCNN_LSTM_CRFExecutor(config)
     executor.run()
 
 
 if __name__ == "__main__":
-    ap = argparse.ArgumentParser(description="Type CNN-LSTM-CRF Model for Sequence Labeling")
-    ap.add_argument("--name", type=str, default="type-cnn-lstm-crf",
-                    help="model name (Default: 'type-cnn-lstm-crf')")
-    ap.add_argument("--checkpoint_dir", type=str, default="../../checkpoints",
-                    help="checkpoints directory (Default: '../../checkpoints')")
-    ap.add_argument("--eval", type=str, default="none",
-                    help="only evaluate existing checkpoint model (none/best/<checkpoint-id>) (Default: 'none')")
-    ap.add_argument("--query", action="store_true",
-                    help="query mode, can be used with eval to work with best model (Default: False)")
-
-    ap.add_argument("--data_dir", type=str, default="../../data/GENIA_term_3.02",
-                    help="path to input dataset directory (Default: '../../data/GENIA_term_3.02')")
-    ap.add_argument("--out_dir", type=str, default="../../data/GENIA_term_3.02/out",
-                    help="path to output directory (Default: '../../data/GENIA_term_3.02/out')")
-    ap.add_argument("--train_path", type=str, default="train.tsv",
-                    help="path to train dataset (train.tsv|std_train.tsv|jnlpba_train.tsv) (Default: 'train.tsv')")
-    ap.add_argument("--dev_path", type=str, default="dev.tsv",
-                    help="path to dev dataset (dev.tsv|std_dev.tsv|jnlpba_dev.tsv) (Default: 'dev.tsv')")
-    ap.add_argument("--test_path", type=str, default="test.tsv",
-                    help="path to test dataset (test.tsv|std_test.tsv|jnlpba_test.tsv) (Default: 'test.tsv')")
-    ap.add_argument("--word_vocab_path", type=str, default="glove_vocab.txt",
-                    help="path to word vocab (Default: 'glove_vocab.txt')")
-    ap.add_argument("--tags_path", type=str, default="tag_vocab.txt",
-                    help="path to output tags vocab. Use 'tag_vocab.txt' for full tags vocab. "
-                         "Use 'std_tag_vocab.txt' for standard 5 tags vocab. "
-                         "Use 'jnlpba_tag_vocab.tsv' for exact (5-tag) settings used by MTL-BioInformatics-2016 "
-                         "(ref: https://github.com/cambridgeltl/MTL-Bioinformatics-2016)"
-                         "Use 'out_freq_tag_vocab.txt' for reduced tags, when considering input tags information. "
-                         "(Default: 'tag_vocab.txt')")
-    ap.add_argument("--out_tag_names_path", type=str, default="tag_names.txt",
-                    help="path to output tag general names. Use 'tag_names.txt' for full tags vocab names. "
-                         "Use 'std_tag_names.txt' for standard 5 tags vocab names. "
-                         "Use 'jnlpba_tag_names.txt' for exact (5-tag) settings used by MTL-BioInformatics-2016 "
-                         "(ref: https://github.com/cambridgeltl/MTL-Bioinformatics-2016)"
-                         "Use 'out_freq_tag_names.txt' for reduced tags, when considering input tags information. "
-                         "(Default: 'tag_names.txt')")
-    ap.add_argument("--inp_tag_vocab_path", type=str, default="empty_inp_tag_vocab.txt",
-                    help="path to input tags vocab. Use 'empty_inp_tag_vocab.txt' if don't want to use tag info. "
-                         "Use 'inp_freq_tag_vocab.txt' for specifying default input tag info."
-                         "(Default: 'empty_inp_tag_vocab.txt')")
-    ap.add_argument("--pos_tag_vocab_path", type=str, default="pos_tag_vocab.txt",
-                    help="path to POS tags vocab. (pos_tag_vocab.txt|jnlpba_pos_tag_vocab.txt) "
-                         "(Default: 'pos_tag_vocab.txt')")
-    ap.add_argument("--dep_tag_vocab_path", type=str, default="dep_tag_vocab.txt",
-                    help="path to dependency-parse tags vocab. (dep_tag_vocab.txt|jnlpba_dep_tag_vocab.txt) "
-                         "(Default: 'dep_tag_vocab.txt')")
-    ap.add_argument("--emb_path", type=str, default="../../../../Embeddings/glove.6B.50d.txt",
-                    help="path to pre-trained word embeddings (Default: '../../../../Embeddings/glove.6B.50d.txt')")
-    ap.add_argument("--tag_emb_path", type=str, default="tag_w2v_emb.txt",
-                    help="path to pre-trained tag embeddings, relative to data_dir "
-                         "(jnlpba_tag_w2v_emb.txt|jnlpba_tag_use_emb.txt|jnlpba_tag_full_emb.txt) "
-                         "(std_tag_w2v_emb.txt|std_tag_use_emb.txt|std_tag_full_emb.txt) "
-                         "(tag_w2v_emb.txt|tag_use_emb.txt|tag_full_emb.txt) (Default: 'tag_w2v_emb.txt')")
-
-    ap.add_argument("--num_epochs", type=int, default=500, help="# epochs to train (Default: 500)")
-    ap.add_argument("--batch_size", type=int, default=128, help="batch size (Default: 128)")
-    ap.add_argument("--word_emb_dim", type=int, default=50, help="word embedding dimension (Default: 50)")
-    ap.add_argument("--pos_tag_emb_dim", type=int, default=15, help="POS tag embedding dimension (Default: 15)")
-    ap.add_argument("--dep_tag_emb_dim", type=int, default=15, help="dep-parse tag embedding dimension (Default: 15)")
-    ap.add_argument("--max_word_len", type=int, default=30, help="max. #chars in word (Default: 30)")
-    ap.add_argument("--max_seq_len", type=int, default=60, help="max. #words in sentence (Default: 60)")
-    ap.add_argument("--conv1_dim", type=int, default=128, help="conv1 layer output channels (Default: 128)")
-    ap.add_argument("--hidden_dim", type=int, default=256, help="hidden state dim for LSTM, if used (Default: 256)")
-    ap.add_argument("--use_maxpool", action="store_true",
-                    help="max pool over CNN output to get char embeddings, else does concatenation (Default: False)")
-    ap.add_argument("--use_pos_tag", action="store_true", help="embed POS tag information (Default: False)")
-    ap.add_argument("--use_dep_tag", action="store_true", help="embed dep-parse tag information (Default: False)")
-    ap.add_argument("--use_tag_cosine_sim", action="store_true",
-                    help="compute cosine sim with tag embeddings as additional layer in model (Default: False)")
-    ap.add_argument("--kernel_size", type=int, default=5, help="kernel size for CNN (Default: 5)")
-    ap.add_argument("--num_lstm_layers", type=int, default=1, help="no. of LSTM layers (Default: 1)")
-    ap.add_argument("--use_char", type=str, default="lower",
-                    help="char embedding type (none/lower/all) (Default: 'lower')")
-    ap.add_argument("--use_pattern", type=str, default="condensed",
-                    help="pattern embedding type (none/one-to-one/condensed) (Default: 'condensed')")
-    ap.add_argument("--escape_digits", action="store_true",
-                    help="replace digits(0-9) with 'd' tag in pattern capturing (Default: False)")
-    ap.add_argument("--ignore_word_lengths", action="store_true",
-                    help="ignore word lengths in pattern capturing (Default: False)")
-    ap.add_argument("--no_lstm", action="store_true",
-                    help="don't use LSTM to capture neighbor context. Directly CRF over individual token level CNN "
-                         "(Default: False)")
-    ap.add_argument("--use_tag_info", type=str, default="pretrained",
-                    help="type information (none/self/window/pretrained) (Default: 'pretrained')")
-    ap.add_argument("--use_tfo", type=str, default="none",
-                    help="use transformer (may not use LSTM then). 'simple' creates a basic tfo. "
-                         "'xl' uses TransformerXL model layer. (none|simple|xl) (Default: 'none')")
-    ap.add_argument("--window_size", type=int, default=5,
-                    help="size of context window for type info on either side of current token (Default: 5)")
-    ap.add_argument("--use_word", type=str, default="allenai/scibert_scivocab_uncased",
-                    help="use word(token) embeddings "
-                         "(none|rand|glove|allenai/scibert_scivocab_uncased|bert-base-uncased"
-                         "|../../../resources/biobert_v1.1_pubmed) "
-                         "(Default: allenai/scibert_scivocab_uncased)")
-    ap.add_argument("--use_pre_padding", action="store_true", help="pre-padding for char/word (Default: False)")
-    ap.add_argument("--word_emb_model_from_tf", action="store_true",
-                    help="word embedding generator model is a pretrained tensorflow model. Use 'True' for models like, "
-                         "'../../../resources/biobert_v1.1_pubmed' (Default: False)")
-    ap.add_argument("--use_class_guidance", action="store_true",
-                    help="take guidance through pre-trained class embeddings (Default: False)")
-    ap.add_argument("--fine_tune_bert", action="store_true", help="fine-tune bert embeddings (Default: False)")
-    ap.add_argument("--lr", type=float, default=0.001, help="learning rate (Default: 0.001)")
-    ap.add_argument("--dropout_ratio", type=float, default=0.5, help="dropout ratio (Default: 0.5)")
-    ap.add_argument("--seed", type=int, default=42, help="manual seed for reproducibility (Default: 42)")
-    ap.add_argument("--use_cpu", action="store_true", help="force CPU usage (Default: False)")
-    ap.add_argument("--no_eval_print", action="store_true",
-                    help="don't output verbose evaluation matrices (Default: False)")
+    ap = argparse.ArgumentParser(description="CNN-LSTM-CRF Model for Sequence Labeling")
+    ap.add_argument("--config", default="../configs/config.json", help="config file")
     ap = ap.parse_args()
     main(ap)
