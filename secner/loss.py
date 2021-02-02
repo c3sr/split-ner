0d1c56f49cbc3158a2834232cba10e491b6ee857
Jatin Arora <jatinarora2702@gmail.com>
2021-02-02 00:45:39 -0600

    trying a different implementation of Dice Loss

--- secner/loss.py
+++ secner/loss.py
@@ -4,11 +4,12 @@ import torch.nn.functional as F
 
 
 class DiceLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1e-8):
         super(DiceLoss, self).__init__()
         self.ignore_index = -100
+        self.eps = eps
 
-    def forward(self, logits, labels, mask, eps=1e-8):
+    def old_forward(self, logits, labels, mask, eps=1e-8):
         """Computes the Sørensen–Dice loss.
         Note that PyTorch optimizers minimize a loss. In this
         case, we would like to maximize the dice loss so we
@@ -31,3 +32,21 @@ class DiceLoss(nn.Module):
         cardinality = torch.sum(prob + labels_1_hot * mod_mask, dim=-1)
         dice_loss = (2.0 * (intersection + eps) / (cardinality + eps)).mean()
         return 1.0 - dice_loss
+
+    def forward(self, logits, labels, mask):
+        num_classes = logits.shape[1]
+        labels_mod = labels.clone()
+        labels_mod[labels_mod == self.ignore_index] = 0
+        labels_1_hot = torch.eye(num_classes)[labels_mod].to(logits.device)
+        labels_1_hot *= mask.unsqueeze(-1).repeat(1, num_classes)
+
+        dice_total = 0.
+        for index in range(num_classes):
+            dice_total += self.dice_coefficient_per_class(labels_1_hot[:, index], logits[:, index], mask)
+        return 1. - dice_total / num_classes
+
+    def dice_coefficient_per_class(self, labels, logits, mask):
+        labels_mod = labels * mask
+        logits_mod = torch.sigmoid(logits) * mask
+        intersection = (labels_mod * logits_mod).sum()
+        return (2. * intersection + self.eps) / (labels_mod.sum() + logits_mod.sum() + self.eps)
