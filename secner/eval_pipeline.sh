d2bd0218f6c80ae88d5203fc3383425ad2105a5e
Y Park <pkyj95@hotmail.com>
2021-06-23 10:54:20 -0500

    run script

--- secner/eval_pipeline.sh
+++ secner/eval_pipeline.sh
@@ -1,1 +1,1 @@
-python analysis.py --dataset bio --model class_scibert_qa_char_pattern3_subtext-dim50  --file infer --only_f1
+python analysis.py --dataset $1 --model $2  --file infer --only_f1
--- secner/eval_span.sh
+++ secner/eval_span.sh
@@ -1,2 +1,2 @@
 python main_qa.py --config $1 --output "$2"
-python analysis.py --data bio --model "$2" --file test --only_f1 --span_based
+python analysis.py --datapath "../models" --dataset conll --model "$2" --file test --only_f1 --span_based
